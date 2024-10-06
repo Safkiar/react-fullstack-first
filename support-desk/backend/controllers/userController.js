ec64ee7308b00c74ce2ae339faf049b9d7100467
Safkiar <m.kikowski97@gmail.com>
2024-10-06 21:45:35 +0200

    added tickets

--- support-desk/backend/controllers/userController.js
+++ support-desk/backend/controllers/userController.js
@@ -49,7 +49,7 @@ const  registerUser = asyncHandler(async (req,res) => {
 const loginUser = asyncHandler(async (req,res) => {
     const {email,password} = req.body
     const user = await User.findOne({email})
-    if(user && (await b.crypt.compare(password, user.password))) {
+    if(user && (await bcrypt.compare(password, user.password))) {
         res.status(200).json({
             _id: user._id,
             name: user.Name,
